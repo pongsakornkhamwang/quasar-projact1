7eaf33615c4a1857686ed1dd1770b749aa440ba5
phngskrkhawang@gmail.com <>
2024-07-24 22:38:16 +0700

    Add script part

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="q-pa-md" style="max-width: 400px">
-
     <q-form
       @submit="onSubmit"
       @reset="onReset"
@@ -9,12 +8,11 @@
       <q-input
         filled
         v-model="name"
-        label="ชื่อนามสกุล *"
-        hint="ชื่อและนามสกุล"
+        label="Your name *"
+        hint="Name and surname"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'ชื่อนามสกุล']"
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
-
       <q-input
         filled
         type="number"
@@ -26,19 +24,12 @@
           val => val > 0 && val < 100 || 'Please type a real age'
         ]"
       />
-
       <q-toggle v-model="accept" label="I accept the license and terms" />
-
-      <div>
-        <q-btn label="Submit" type="submit" color="primary"/>
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
-      </div>
       <div>
         <q-btn label="Submit" type="submit" color="primary"/>
         <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
-
   </div>
 </template>
 
